af41f825a081f772bc0e33a8fc306a6664536d05
Muhammad Awais <awaisshaikh94@gmail.com>
2023-09-10 11:34:45 +0500

    fixes

--- src/app/pages/products/products/products.component.ts
+++ src/app/pages/products/products/products.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+
+  data: any = [];
+
+  constructor(
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe((res: any) => {
+      this.data = res;
+
+      this.data = this.data.map((i: Product) => {
+        return {
+          ...i,
+          description: `${i.description.substring(0, 60)}...`
+        }
+      });
+
+      console.log(res);
+    })
+  }
 
 }
