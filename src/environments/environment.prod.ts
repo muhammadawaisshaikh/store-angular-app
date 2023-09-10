94822e67769ff49fb732eb3c08ab33f936d74444
Muhammad Awais <awaisshaikh94@gmail.com>
2023-09-11 00:31:21 +0500

    prod base url fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://fakestoreapi.com/',
-  AUTH_BASE_URL: 'https://demo1416688.mockable.io/'
+  API_BASE_URL: 'https://fakestoreapi.com',
+  AUTH_BASE_URL: 'https://demo1416688.mockable.io'
 };
